6ee00ebf57f164b0519f286279efd4c53d1aa3f0
Tomasz 'Zen' Napierała <tomasz@napierala.org>
2018-09-22 05:16:49 -0700

    Revert "updating options (#51)"
    This reverts commit 22861b943a193a00e9c7988b75718e0c378336c6.

--- zigbee2mqtt/run.sh
+++ zigbee2mqtt/run.sh
@@ -3,13 +3,18 @@
 CONFIG_PATH=/data/options.json
 
 DATA_PATH=$(jq --raw-output ".data_path" $CONFIG_PATH)
-
-ZIGBEE_SHEPHERD_DEBUG=$(jq --raw-output ".zigbee_shepherd_debug // empty" $CONFIG_PATH)
+DEBUG_ZIGBEE2MQTT=$(jq --raw-output ".debug // empty" $CONFIG_PATH)
+ERR_LOG=$(jq --raw-output ".err // empty" $CONFIG_PATH)
 
 python3 set_config.py "$CONFIG_PATH" "$DATA_PATH"
 
-if [[ ! -z "$ZIGBEE_SHEPHERD_DEBUG" ]]; then
-    export DEBUG="zigbee-shepherd*" 
+if [[ ! -z "$DEBUG_ZIGBEE2MQTT" ]]; then
+    export DEBUG=*
 fi
 
-ZIGBEE2MQTT_DATA="$DATA_PATH" pm2-runtime start npm -- start
+
+if [[ ! -z "$ERR_LOG" ]]; then
+    ZIGBEE2MQTT_DATA="$DATA_PATH" npm start 1> "$DATA_PATH"/out.log 2> "$DATA_PATH"/err.log
+else
+    ZIGBEE2MQTT_DATA="$DATA_PATH" pm2-runtime start npm -- start
+fi
